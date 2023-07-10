8ea8af8d4619e5c8dc7d0e1fdeb41cdecea90943
sebastiankot <sebaa.kot@gmail.com>
2023-07-10 16:17:17 +0200

    Init project

---
+++ src/main/java/kots/weatherforecastapp/WeatherForecastAppApplication.java
@@ -0,0 +1,13 @@
+package kots.weatherforecastapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kots/weatherforecastapp/WeatherForecastAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kots.weatherforecastapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
